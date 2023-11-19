cffcf5fd9f93da1494a940b45575cb17741f4945
Darkest08 <karpenkom2003@gmail.com>
2023-11-19 17:00:19 +0300

    + нахождеие размера файла и памяти

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#define _WIN32_WINNT  0x0501
+#include <windows.h>
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <math.h>
 using namespace std;
+
+ifstream::pos_type filesize(string & filename)
+{
+    ifstream in(filename, ifstream::binary);
+    return in.tellg(); 
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string inpath = "input.bi";
+
+    unsigned int fsize = filesize(inpath); //размер читаемого файла
+        MEMORYSTATUSEX statex;
+        statex.dwLength = sizeof (statex);
+        GlobalMemoryStatusEx (&statex);
+    unsigned int global_msize = (int)statex.ullTotalPhys; //размер оперативной памяти
+    unsigned int msize = ceil(global_msize / 4); //размер доступной для кучи оперативной памяти
+
     return 0;
 }
