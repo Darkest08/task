e622410ca0222abf9e20f91d2086da0056efef75
Darkest08 <karpenkom2003@gmail.com>
2023-11-19 17:00:51 +0300

    генератор тестового файла

---
+++ input_generator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+
+int main()
+{
+    srand(time(nullptr));
+    ofstream fout("input.bi");
+    unsigned long int size = 5;
+    for (unsigned long int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < 32; ++j)
+        {
+            fout << (bool)(rand() % 2);
+        }
+    }
+    fout.close();
+    return 0;
+}
