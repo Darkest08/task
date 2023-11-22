0bac986e81d729ecff7e5a47a811225b95ec03c4
Darkest08 <karpenkom2003@gmail.com>
2023-11-22 12:22:55 +0300

    Добавил программу чтения для проверки

---
+++ reader.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include <math.h>
+#include <fstream>
+using namespace std;
+
+int glenth = 32;
+
+int main()
+{
+    string path;
+    cin >> path;
+    ifstream in(path);
+    int eof = 0;
+    do 
+    {
+    int val = 0;
+    char buf = 0;
+    int mul = 1;
+    buf = in.get();
+    if (!in.eof())
+    {
+        if (buf == '1') 
+        {
+            mul = -1;
+        }
+        for (int i = 1; i < glenth; ++i)
+        {
+            buf = in.get();
+            if (in.eof())
+                {
+                    eof = 1;
+                    break;
+                }
+            if (buf == '1')
+                val+=pow(2,glenth - i - 1);
+        }
+    }
+    else
+    {
+        eof = 1;
+    }
+    cout << val * mul << ' ';
+    }
+    while(eof != 1);
+
+    cout << endl;
+    return 0;
+}
