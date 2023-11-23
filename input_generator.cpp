af1c2ade0eae8424f653b41577f1d0b7ab2c56ef
Darkest08 <karpenkom2003@gmail.com>
2023-11-23 14:12:40 +0300

    исправил генерацую для создания бинарного файла

--- input_generator.cpp
+++ input_generator.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-
+#include <math.h>
 using namespace std;
 
 
 int main()
 {
     srand(time(nullptr));
-    ofstream fout("input.bi");
+    ofstream fout("input.bin", ios::binary);
+    int buf = 0;
+    int mul = -1;
     unsigned long int size = 50;
     for (unsigned long int i = 0; i < size; ++i)
     {
-        for (int j = 0; j < 32; ++j)
+        if ((rand() % 2) == 1)
+            mul = -1;
+        for (int j = 0; j < 31; ++j)
         {
-            fout << (bool)(rand() % 2);
+            buf += (rand() % 2) * pow (2,j);
         }
+        fout.write((char*)&buf, sizeof(buf));
     }
     fout.close();
     return 0;
