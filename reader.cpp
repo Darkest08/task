1ea58f13240875dd75dbd7165a2a357f73d77624
Darkest08 <karpenkom2003@gmail.com>
2023-11-26 20:03:16 +0300

    ^

--- reader.cpp
+++ reader.cpp
@@ -8,33 +8,28 @@ int glenth = 32;
 
 int main()
 {
+    cout<< "Enter the path" << endl;
     string path;
     cin >> path;
-    ifstream in(path);
+    ifstream in(path,  ios::out | ios::binary);
     int eof = 0;
     int size = 0; //число чисел
-    do 
-    {
-        int val = 0;
-        in.read((char*)&val, sizeof(val));
-        if (!in.eof())
-        {
-            ++size;
-            cout << val << ' ';
-        }
-        else
-        {
-            eof = 1;
-        }
-    }
-    while(eof != 1);
     FILE *fp = fopen(path.c_str(), "r");
     fseek(fp, 0L, SEEK_END);
     int size_ = ftell(fp);       // размер (число позиций)
     rewind(fp);
     fclose(fp);
+    for (int i = 0; i < size_ / 4; ++i)
+    {
+        int val = 0;
+        in.read((char*)&val, sizeof(val));
+        ++size;
+        cout << val << ' ';
+    }
+    in.close();
+
     cout << endl << size << " " << size_ << endl;
     
-    in.close();
+    
     return 0;
 }
